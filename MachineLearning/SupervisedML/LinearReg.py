017223f19e3f43149ea0a67ae783cec5a8bfff8c
Shivam1848 <kashokagrawal1848@gmail.com>
2026-06-27 23:56:15 +0530

    Understanding overfit and underfit

--- MachineLearning/SupervisedML/LinearReg.py
+++ MachineLearning/SupervisedML/LinearReg.py
@@ -82,3 +82,8 @@ y_pred = model.predict(X_test)
 # Evaluting Model
 r2 = r2_score(Y_test, y_pred)
 print(r2)
+
+## Analyzing Underfit or Overfit
+y_train_pred = model.predict(X_train)
+r2_train = r2_score(Y_train, y_train_pred)
+print(r2_train)
